0b5b16ecd704307149fad78f6121fd7914d09eb8
“tmk-a” <tamaki.akita.jp@gmail.com>
2025-09-11 18:48:42 -0700

    feat: Implement add new task feature

---
+++ frontend/src/components/TodoInput.tsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../redux/todoSlice";
+const TodoInput = () => {
+  const [title, setTitle] = useState("");
+  const [dec, setDec] = useState("");
+  const [status, setStatus] = useState("todo");
+  const [priority, setPriority] = useState("low");
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // set id
+    const id =
+      Date.now().toString(36) + Math.random().toString(36).substring(2, 7);
+
+    dispatch(addTodo({ id, title, dec, status, priority }));
+
+    // clear input
+    setTitle("");
+    setDec("");
+    setStatus("todo");
+    setPriority("low");
+  };
+
+  return (
+    <form
+      className="flex flex-col gap-3 border-gray-300 border-2 p-4"
+      onSubmit={handleSubmit}
+    >
+      <h2>Create Task</h2>
+      <div className="flex items-center gap-2">
+        <input
+          type="text"
+          placeholder="Type title..."
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="w-full px-2 py-1"
+        />
+      </div>
+
+      <div className="flex items-center gap-2">
+        <input
+          type="text"
+          placeholder="Type description..."
+          name="dec"
+          value={dec}
+          onChange={(e) => setDec(e.target.value)}
+          className="w-full px-2 py-1"
+        />
+      </div>
+
+      <div className="flex flex-col gap-2 sm:flex-row md:gap-4">
+        <div className="flex items-center gap-2">
+          <label htmlFor="status" className="text-sm font-medium text-gray-600">
+            Status:
+          </label>
+          <select
+            id="status"
+            name="status"
+            value={status}
+            onChange={(e) => setStatus(e.target.value)}
+          >
+            <option value="todo">Todo</option>
+            <option value="in-progress">In-Progress</option>
+            <option value="done">Done</option>
+          </select>
+        </div>
+        <div className="flex items-center gap-2">
+          <label htmlFor="status" className="text-sm font-medium text-gray-600">
+            Priority:
+          </label>
+          <select
+            id="priority"
+            name="priority"
+            value={priority}
+            onChange={(e) => setPriority(e.target.value)}
+          >
+            <option value="low">Low</option>
+            <option value="medium">Medium</option>
+            <option value="high">High</option>
+          </select>
+        </div>
+      </div>
+      <button
+        type="submit"
+        className="border-gray-300 border-2 bg-green-900 text-white hover:bg-green-700 cursor-pointer"
+      >
+        Add
+      </button>
+    </form>
+  );
+};
+
+export default TodoInput;
